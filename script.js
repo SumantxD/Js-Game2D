08bc63acbe1b85e5da33c222ef6e30bb4b7e9856
SumantxD <st72365@gmail.com>
2022-12-01 06:10:57 +0530

    the dog added and positioned correctly

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ playerImage.src = 'shadow_dog.png';
 
 // let x = 0;
 
+const spriteWidth = 575;
+const spriteHeight = 523;
+
 function  animate(){
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(100,50,100,100);
+    // ctx.fillRect(100,50,100,100);
     // x++;
-    ctx.drawImage(playerImage, 0, 0);
+    // ctx.drawImage(playerImage, 0, 0);
+
+    //from where we want to cut from the sprite image //and where do we want to place it in our canvas
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0,0, spriteWidth, spriteHeight);
+
     requestAnimationFrame(animate);
 }
 animate();
