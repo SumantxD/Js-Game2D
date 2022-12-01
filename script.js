d9d71f19d5f2f57ea8358135d0b68b2d9576014f
SumantxD <st72365@gmail.com>
2022-12-01 06:04:24 +0530

    the dog sprite displayed

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ const CANVAS_HEIGHT = canvas.height = 600;
 const playerImage = new Image();//we have createed new image object
 playerImage.src = 'shadow_dog.png';
 
-let x = 0;
+// let x = 0;
 
 function  animate(){
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(x,50,100,100);
-    x++;
+    ctx.fillRect(100,50,100,100);
+    // x++;
+    ctx.drawImage(playerImage, 0, 0);
     requestAnimationFrame(animate);
 }
 animate();
